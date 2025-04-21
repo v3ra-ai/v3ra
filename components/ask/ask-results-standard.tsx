aca6df7c9d7aab5144e738053cec12dc56d47d7f
Chris St. John <csjcode@gmail.com>
2025-04-21 17:10:07 -0600

    feat: update Card component width for improved layout in grid mode

--- components/ask/ask-results-standard.tsx
+++ components/ask/ask-results-standard.tsx
@@ -91,7 +91,7 @@ export default function AskResultsStandard() {
               <Card
                 key={query.id}
                 className={`bg-white dark:bg-zinc-800 ${
-                  layoutMode === "grid" ? "w-full md:w-[22rem]" : "w-full lg:w-4xl"
+                  layoutMode === "grid" ? "w-full lg:w-[22rem]" : "w-full lg:w-4xl"
                 }`}
               >
                 <CardHeader>
