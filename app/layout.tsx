c67302bef1f4fa403f5063a48a867847554986d1
Chris St. John <csjcode@gmail.com>
2025-04-28 09:59:13 -0600

    fix: change default theme to dark in RootLayout; add new images for content and icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
       <body className={inter.className}>
         <ThemeProvider
           attribute="class"
-          defaultTheme="system"
+          defaultTheme="dark"
           enableSystem
           disableTransitionOnChange
         >
