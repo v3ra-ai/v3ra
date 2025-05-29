ef8c736b64474f468efc17b66bf14d6ed873597f
Chris St. John <csjcode@gmail.com>
2025-05-28 20:33:43 -0600

    feat(ask-results): wrap AskResultsStandardTitle in Link for navigation

--- components/ask/results/ask-results-standard-title.tsx
+++ components/ask/results/ask-results-standard-title.tsx
@@ -1,6 +1,7 @@
 import { CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import { CircleCheck, X } from "lucide-react";
 import { VoteResult } from "@/lib/types";
+import Link from "next/link";
 
 interface AskResultsStandardTitleProps {
   sanitizedQuery: VoteResult;
@@ -10,19 +11,26 @@ export function AskResultsStandardTitle({
   sanitizedQuery,
 }: AskResultsStandardTitleProps) {
   return (
-<CardHeader className="dark:bg-zinc-800">
-<CardDescription className="flex font-light text-xs dark:text-zinc-500 text-zinc-500"></CardDescription>      <CardTitle className="text-lg font-medium flex">
-        <div className="">
-          {sanitizedQuery.isConsensusReached &&
-          sanitizedQuery.consensusValue ? (
-            <CircleCheck className="mr-2 h-7 w-7 text-green-700 dark:text-green-300" />
-          ) : sanitizedQuery.isConsensusReached &&
-            !sanitizedQuery.consensusValue ? (
-            <X className="mr-2 h-7 w-7 text-red-700 dark:text-red-300" />
-          ) : null}
-        </div>
-        <div>{sanitizedQuery.queryText}</div>
-      </CardTitle>
-    </CardHeader>
+    <Link
+      href={`/ask/${sanitizedQuery.id}`}
+      className="text-zinc-900 dark:text-zinc-100"
+    >
+      <CardHeader className="dark:bg-zinc-800">
+        <CardDescription className="flex font-light text-xs dark:text-zinc-500 text-zinc-500"></CardDescription>
+        <CardTitle className="text-xl font-medium flex">
+          <div className="">
+            {sanitizedQuery.isConsensusReached &&
+            sanitizedQuery.consensusValue ? (
+              <CircleCheck className="mr-2 h-7 w-7 text-green-700 dark:text-green-300" />
+            ) : sanitizedQuery.isConsensusReached &&
+              !sanitizedQuery.consensusValue ? (
+              <X className="mr-2 h-7 w-7 text-red-700 dark:text-red-300" />
+            ) : null}
+          </div>
+
+          {sanitizedQuery.queryText}
+        </CardTitle>
+      </CardHeader>
+    </Link>
   );
 }
